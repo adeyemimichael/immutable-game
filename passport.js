946f3b7787460aefabbd9a8470b3a19b2a62061b
adeyemimike123 <ayobami732000@gmail.com>
2023-12-15 22:08:35 +0100

    updated the passport.js

--- passport.js
+++ passport.js
@@ -2,7 +2,7 @@ window.passport = new window.immutable.passport.Passport({
     baseConfig: new window.immutable.config.ImmutableConfiguration({
       environment: window.immutable.config.Environment.SANDBOX,
     }),
-    clientId: 'Qa3ljM2L3OuezNEQ6GRbk5tBlQWod3hF',
+    clientId: 'F9TTK78VeQhuEJXql9EL3LhT7QgLqDDx',
     redirectUri: 'https://comic-deadly-bluegill.ngrok-free.app',
     logoutRedirectUri: 'https://comic-deadly-bluegill.ngrok-free.app/logout.html',
     audience: 'platform_api',
