007dc9d46eccf22501e0d078ea01449d1f6bf019
adeyemimike123 <ayobami732000@gmail.com>
2023-12-15 11:05:06 +0100

    first commit

--- passport.js
+++ passport.js
@@ -2,9 +2,9 @@ window.passport = new window.immutable.passport.Passport({
     baseConfig: new window.immutable.config.ImmutableConfiguration({
       environment: window.immutable.config.Environment.SANDBOX,
     }),
-    clientId: 'enter-client-id',
-    redirectUri: 'redirect-url',
-    logoutRedirectUri: 'logout-url',
+    clientId: 'Qa3ljM2L3OuezNEQ6GRbk5tBlQWod3hF',
+    redirectUri: 'https://comic-deadly-bluegill.ngrok-free.app',
+    logoutRedirectUri: 'https://comic-deadly-bluegill.ngrok-free.app/logout.html',
     audience: 'platform_api',
     scope: 'openid offline_access email transact'
   });
